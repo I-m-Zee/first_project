4040c9a7c18ef292b948bb53decde84881e9ec5f
I-m-Zee <bc090402389@gmail.com>
2023-07-25 11:21:18 +0500

    events & Parameters

--- src/components/first-component.vue
+++ src/components/first-component.vue
@@ -6,6 +6,12 @@
     <h1>Name: {{ getName("Your Name") }}</h1>
     <h1>Data Email:  {{ getData().email }}</h1>
     <h1>Data Number:  {{ getData().mobile }}</h1>
+    <!-- Frist way to capture events -->
+    <button @click="click()">Click Here</button>
+    <h1 @mousemove="callConsole()">Home Page</h1>
+    <!-- Second way to capture events -->
+    <!-- <button v-on:click="click()">Click Here</button>
+    <h1 v-on:mousemove="callConsole()">Home Page</h1> -->
 </template>
 <script>
 // Working in Same Compoenent
@@ -26,6 +32,12 @@ export default {
                 email: "abc@gmail.com",
                 mobile: "9562256",
             }
+        },
+        click() {
+            alert("How Are you?")
+        },
+        callConsole() {
+            console.error("Hey There You are!")
         }
     }
 }
