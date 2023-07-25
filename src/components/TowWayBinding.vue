590ab7f2b1004d3c228f299950a92b710b8e86e4
I-m-Zee <bc090402389@gmail.com>
2023-07-25 12:53:19 +0500

    mutltiple Input Two way bindings

--- src/components/TowWayBinding.vue
+++ src/components/TowWayBinding.vue
@@ -1,6 +1,10 @@
 <script>
+import MultipleTwoWayBinding from './multiple-twoway-binding.vue'
 export default {
     name: "TwoWay",
+    components:{
+        MultipleTwoWayBinding
+    },
     data() {
         return {
             count:5895,
@@ -12,6 +16,8 @@ export default {
     <h1>Two Way Binding</h1>
     <input type="text" name="twoWay" id="twoWay" placeholder="Write Number here" v-model="count">
     <h2>{{ count }}</h2>
+    <hr>
+    <MultipleTwoWayBinding/>
 </template>
 <style scoped>
 h1{
---
+++ src/components/multiple-twoway-binding.vue
@@ -0,0 +1,60 @@
+<script>
+export default {
+    name: "MultipleTwoWayBinding",
+    data() {
+        return {
+            selectedSports: [],
+            selectedPlayer:''
+        }
+    }
+}
+</script>
+<template>
+    <h1>Multiple Two Way Binding</h1>
+    <label for="sport" class="sport">Select Sports</label>
+    <br>
+    <label for="cricket">Cricket</label>
+    &nbsp;
+    <input type="checkbox" name="cricket" id="cricket" v-model="selectedSports" value="cricket">
+    <br>
+    <label for="football">FootBall</label>
+    &nbsp;
+    <input type="checkbox" name="football" id="football" v-model="selectedSports" value="football">
+    <br>
+    <label for="vollyBall">Volly Ball</label>
+    &nbsp;
+    <input type="checkbox" name="vollyBall" id="vollyBall" v-model="selectedSports" value="vollyBall">
+    <br>
+    <label for="rugby">Rugby</label>
+    &nbsp;
+    <input type="checkbox" name="rugby" id="rugby" v-model="selectedSports" value="rugby">
+    <br>
+    <label for="name">Select Player</label>
+    <br>
+    <label for="first">First</label>
+    &nbsp;
+    <input type="radio" name="selectedPlayer" id="name" v-model="selectedPlayer" value="First">
+    <br>
+    <label for="second">Second</label>
+    &nbsp;
+    <input type="radio" name="selectedPlayer" id="name" v-model="selectedPlayer" value="Second">
+    <br>
+    <label for="third">Third</label>
+    &nbsp;
+    <input type="radio" name="selectedPlayer" id="name" v-model="selectedPlayer" value="Third">
+    <br>
+    <label for="fourth">Fourth</label>
+    &nbsp;
+    <input type="radio" name="selectedPlayer" id="name" v-model="selectedPlayer" value="Fourth">
+    <div>
+        <h2>Selected Sports are: {{ selectedSports }}</h2>
+        <h2>Selected Player is: {{ selectedPlayer }}</h2>
+    </div>
+</template>
+<style>
+.sport {
+    font-size: x-large;
+    color: blue;
+    font-weight: bold;
+}
+</style>
